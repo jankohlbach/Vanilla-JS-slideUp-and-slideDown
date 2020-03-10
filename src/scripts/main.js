118870897fcf9d0ebc2bb3bbbc200accd066ab22
Jan Kohlbach <mail@jankohlbach.dev>
2020-03-10 09:23:27 +0100

    add additional, optional parameter
    - add optional active class
    - add optional animation time
    - add optional easing selection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,93 +1,137 @@
-(() => {
-  const ACTIVE_CLASS = 'open';
+const easings = {
+  linear(t) {
+    return t;
+  },
+  ease(t) {
+    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+  },
+  easeCubic(t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+  },
+};
 
-  const getHeight = (element) => {
-    let { height, paddingTop, paddingBottom } = getComputedStyle(element);
-    height = parseInt(height, 10);
-    paddingTop = parseInt(paddingTop, 10);
-    paddingBottom = parseInt(paddingBottom, 10);
-    return { height, paddingTop, paddingBottom };
+const getHeight = (element) => {
+  let { height, paddingTop, paddingBottom } = getComputedStyle(element);
+  height = parseInt(height, 10);
+  paddingTop = parseInt(paddingTop, 10);
+  paddingBottom = parseInt(paddingBottom, 10);
+  return { height, paddingTop, paddingBottom };
+};
+
+const setTime = height => height / 3 + 250;
+
+/**
+ * @param {node} element
+ * @param {string} activeClass
+ * @param {number} duration [ms]
+ * @param {string} timingFunction one of [linear, ease, easeCubic]
+ */
+const slideDown = (element, activeClass, duration, timingFunction = 'ease') => {
+  // calculate height
+  element.setAttribute('style', 'display: block;');
+  const { height, paddingTop, paddingBottom } = getHeight(element);
+  element.setAttribute('style', 'display: none;');
+
+  // set time
+  let time;
+  if (duration !== undefined && typeof duration === 'number') {
+    time = duration;
+  } else {
+    time = setTime(height);
+  }
+
+  // set activeClass
+  if (activeClass !== undefined && typeof activeClass === 'string') element.parentNode.classList.add(activeClass);
+
+  element.setAttribute('style', 'overflow: hidden; display: block; padding-top: 0; padding-bottom: 0;');
+
+  const initTime = new Date().getTime();
+  const repeat = () => {
+    const newTime = new Date().getTime() - initTime;
+    const timestep = newTime / time;
+    const timefactor = easings[timingFunction](timestep);
+    const step = 0 + height * timefactor;
+    const stepPaddingT = 0 + (paddingTop * timefactor);
+    const stepPaddingB = 0 + (paddingBottom * timefactor);
+
+    if (newTime <= time && Math.ceil(step) < height) {
+      element.setAttribute('style', `overflow: hidden; display: block; padding-top: ${stepPaddingT}px; padding-bottom: ${stepPaddingB}px; height: ${step}px`);
+    } else {
+      element.setAttribute('style', 'display: block');
+    }
+
+    const repeatLoop = requestAnimationFrame(repeat);
+
+    if (Math.ceil(step) >= height) {
+      cancelAnimationFrame(repeatLoop);
+    }
   };
 
-  const setTime = height => height / 3 + 150;
+  repeat();
+};
 
-  const slideDown = (element) => {
-    element.setAttribute('style', 'display: block;');
-    const { height, paddingTop, paddingBottom } = getHeight(element);
-    element.setAttribute('style', 'display: none;');
-    const time = setTime(height);
+/**
+ * @param {node} element
+ * @param {string} activeClass
+ * @param {number} duration [ms]
+ * @param {string} timingFunction one of [linear, ease, easeCubic]
+ */
+const slideUp = (element, activeClass, duration, timingFunction = 'ease') => {
+  // calculate height
+  const { height, paddingTop, paddingBottom } = getHeight(element);
 
-    element.parentNode.classList.add(ACTIVE_CLASS);
-    element.setAttribute('style', 'overflow: hidden; display: block; padding-top: 0; padding-bottom: 0;');
+  // set time
+  let time;
+  if (duration !== undefined && typeof duration === 'number') {
+    time = duration;
+  } else {
+    time = setTime(height);
+  }
 
-    const initTime = new Date().getTime();
-    const repeat = () => {
-      const newTime = new Date().getTime() - initTime;
-      const step = 0 + height * newTime / time;
-      const stepPaddingT = 0 + (paddingTop * newTime / time);
-      const stepPaddingB = 0 + (paddingBottom * newTime / time);
+  // remove activeClass
+  if (activeClass !== undefined && typeof activeClass === 'string') element.parentNode.classList.remove(activeClass);
 
-      if (newTime <= time) {
-        element.setAttribute('style', `overflow: hidden; display: block; padding-top: ${stepPaddingT}px; padding-bottom: ${stepPaddingB}px; height: ${step}px`);
-      } else {
-        element.setAttribute('style', 'display: block');
-      }
+  element.setAttribute('style', 'overflow: hidden; display: block; padding-top: 0; padding-bottom: 0;');
 
-      const repeatLoop = requestAnimationFrame(repeat);
+  const initTime = new Date().getTime();
+  const repeat = () => {
+    const newTime = new Date().getTime() - initTime;
+    const timestep = newTime / time;
+    const timefactor = easings[timingFunction](timestep);
+    const step = height + -height * timefactor;
+    const stepPaddingT = paddingTop + (-paddingTop * timefactor);
+    const stepPaddingB = paddingBottom + (-paddingBottom * timefactor);
 
-      if (Math.floor(step) > height) {
-        cancelAnimationFrame(repeatLoop);
-      }
-    };
+    if (newTime <= time && Math.floor(step) > 0) {
+      element.setAttribute('style', `overflow: hidden; display: block; padding-top: ${stepPaddingT}px; padding-bottom: ${stepPaddingB}px; height: ${step}px`);
+    } else {
+      element.setAttribute('style', 'display: none');
+    }
 
-    repeat();
+    const repeatLoop = requestAnimationFrame(repeat);
+
+    if (Math.floor(step) <= 0) {
+      cancelAnimationFrame(repeatLoop);
+    }
   };
 
-  const slideUp = (element) => {
-    const { height, paddingTop, paddingBottom } = getHeight(element);
-    const time = setTime(height);
+  repeat();
+};
 
-    element.parentNode.classList.remove(ACTIVE_CLASS);
-    element.setAttribute('style', 'overflow: hidden; display: block; padding-top: 0; padding-bottom: 0;');
-
-    const initTime = new Date().getTime();
-    const repeat = () => {
-      const newTime = new Date().getTime() - initTime;
-      const step = height + -height * newTime / time;
-      const stepPaddingT = paddingTop + (-paddingTop * newTime / time);
-      const stepPaddingB = paddingBottom + (-paddingBottom * newTime / time);
-
-      if (newTime <= time) {
-        element.setAttribute('style', `overflow: hidden; display: block; padding-top: ${stepPaddingT}px; padding-bottom: ${stepPaddingB}px; height: ${step}px`);
-      } else {
-        element.setAttribute('style', 'display: none');
-      }
-
-      const repeatLoop = requestAnimationFrame(repeat);
-
-      if (Math.floor(step) < 0) {
-        cancelAnimationFrame(repeatLoop);
-      }
-    };
-
-    repeat();
-  };
-
-  const toggleHeads = document.querySelectorAll('.toggle-head');
-  toggleHeads.forEach((toggleHead) => {
-    toggleHead.addEventListener('click', () => {
-      if (!toggleHead.parentNode.classList.contains(ACTIVE_CLASS)) {
-        slideDown(toggleHead.nextElementSibling);
-      } else {
-        slideUp(toggleHead.nextElementSibling);
-      }
-    });
+const toggleHeads = document.querySelectorAll('.toggle-head');
+toggleHeads.forEach((toggleHead) => {
+  toggleHead.addEventListener('click', () => {
+    if (!toggleHead.parentNode.classList.contains('active')) {
+      slideDown(toggleHead.nextElementSibling, 'active', '', 'ease');
+    } else {
+      slideUp(toggleHead.nextElementSibling, 'active');
+    }
   });
+});
 
-  const toggleFooter = document.querySelectorAll('.toggle-foot');
-  toggleFooter.forEach((toggleFoot) => {
-    toggleFoot.addEventListener('click', () => {
-      slideUp(toggleFoot.parentNode);
-    });
+const toggleFooter = document.querySelectorAll('.toggle-foot');
+toggleFooter.forEach((toggleFoot) => {
+  toggleFoot.addEventListener('click', () => {
+    slideUp(toggleFoot.parentNode, 'active', 600, 'linear');
   });
-})();
+});
